104223828938a22a041f497fef761cba3b89f9c3
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-03-19 19:10:54 -0300

    Calculadora e Lista de convidado
    Aula 1 C# - Devaria (2021)

---
+++ Lista de Convidado/Lista de Convidado/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ListaDeConvidade
+{
+    class Program
+    {
+        private static string[] ListaDeConvidados =
+        {
+            "Pedro", // 0
+            "Matheus", // 1
+            "João", // 2                    //lista de convidados já definida
+            "Yasmim", // 3
+            "Gabriel", // 4
+            "Ale", // 5
+            "Leitícia" // 6
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o nome do convidado: ");
+            var nome = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(nome) || string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("Nome não informado, insira uma informação válida: ");
+                return;
+            }
+            Console.WriteLine("Informe a idade: ");
+            var idadeString = Console.ReadLine();
+            int idade;
+            bool IdadeInformada = int.TryParse(idadeString, out idade);
+
+            if (IdadeInformada == false)
+            {
+                Console.WriteLine("Idade não informada, insira uma informação válida");
+                return;
+            }
+            bool EstáConvidado;
+            switch (nome)
+            {
+                case "Pedro":
+                    EstáConvidado = true;
+                    break;
+                case "Matheus":
+                    EstáConvidado = true;
+                    break;
+                case "João":
+                    EstáConvidado = true;
+                    break;
+                case "Yasmim":
+                    EstáConvidado = true;
+                    break;
+                case "Gabriel":
+                    EstáConvidado = true;
+                    break;
+                case "Ale":
+                    EstáConvidado = true;
+                    break;
+                case "Leitícia":
+                    EstáConvidado = true;
+                    break;
+                default:
+                    EstáConvidado = false;
+                    break;
+            }
+
+            if (EstáConvidado == true && idade >= 18)
+            {
+                Console.WriteLine("Parabéns seja bem vindo a festa");     // comparações finais do programa
+            }
+            else if (EstáConvidado == false)
+            {
+                Console.WriteLine("Sinto muito você não foi convidado");
+            }
+            else
+            {
+                Console.WriteLine("Você é menor de idade");
+            }
+
+        }
+    }
+}
