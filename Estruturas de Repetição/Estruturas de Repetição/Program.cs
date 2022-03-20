4f6cf95dc1c7c9b1872da19c9b3cc07016b643c0
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-03-20 20:55:53 -0300

    Aula 2 de Csharp
    Aula 2 de C# da Devaria (2021)

---
+++ Estruturas de Repetição/Estruturas de Repetição/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EstruturadeRepetição
+{
+    class Program
+    {
+        static void Main(string[] args)          //informei 6 argumento
+        {
+            if(args.Length == 0)       //aqui conferiu se os argumentos era igual a 0
+            {
+                Console.WriteLine("favor informar palavras no argumento");    //como tinha 6 então era falso, então não entrou aqui
+                return;
+            }
+
+            // 1° FOR
+            for (var i = 0; i < args.Length; i++) // for só existe com 3 clasulas (variável de controle, condição e incremento da var de controle)
+            {
+                Console.WriteLine($"Estrutura for, índice e valor: {i}, {args[i]}");    // $ pra poder colocar a variável
+            }
+
+            // While
+            var argslidos = 0;
+            while (argslidos < args.Length)
+            {
+                Console.WriteLine($"Estrutura while, args e valor lido: {argslidos}, {args[argslidos]}"); //condição checada antes de executar
+                argslidos++;
+            }
+
+            // do while
+            var loopsefetuado = 0;
+            do                                        // primeiro executa e depois checa
+            {
+                Console.WriteLine($"estrutura do while, loops efetuados: {loopsefetuado}, e valor lido: {args[loopsefetuado]} ");   
+                loopsefetuado++;
+            } while (loopsefetuado < args.Length);
+
+            //foreach (ele sempre percorre o tamanho de uma lista)
+            foreach(var argumento in args)
+            {
+                Console.WriteLine($"Estrutura foreach, valor lido: {args}"); //ele sempre parte uma de lista
+            }
+
+
+        }
+    }
+}
---
+++ Lista de Produtos/Lista de Produtos/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace listadecompras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ProdutosDisponiveis = new List<string>()                //criando lista dos produtos
+            {
+                "pão", "leite", "carne", "ovo", "fermento", "arroz", "refrigerante"
+            };
+
+            //validar se os argumentos foram preenchidos
+            try
+            {
+                //listar quais produtos do argumento informado tem na lista do mercado
+                var produtosselecionados = ProdutosDisponiveis.Where(produto => args.Contains(produto)).ToList();        //checa se os produtos estão na lista de argumentos
+                foreach(var produtosselecionado in produtosselecionados)
+                {
+                    Console.WriteLine($"Esse produto nós temos: {produtosselecionado}");
+                }
+
+                //listar quais produtos do argumento informado NÃO tem na lista do mercado
+                var ProdutosNaoDisponiveis = args.Where(args => !ProdutosDisponiveis.Contains(args)).ToList();         //aqui checa quais produtos não tem
+                foreach(var ProdutosNaoDisponivel in ProdutosNaoDisponiveis)
+                {
+                    Console.WriteLine($"Esse produtos não temos infelizmente: {ProdutosNaoDisponivel}");
+                }
+
+                //retorna os produtos que tem na lista do mercado em ordem alfabética
+                var produtodisponivelordenadopornome = ProdutosDisponiveis.OrderBy(e => e).ToList();
+                foreach(var produtoordenado in produtodisponivelordenadopornome)             
+                {
+                    Console.WriteLine($"Estes são os produtos disponíveis: {produtoordenado}");
+                }
+
+            }
+               
+            catch (Exception)
+            {
+                Console.WriteLine("Argumento inválido");
+            }
+            
+          
+
+
+        }
+    }
+
+
+}
