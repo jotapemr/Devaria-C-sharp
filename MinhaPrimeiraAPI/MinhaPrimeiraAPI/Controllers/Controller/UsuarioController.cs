8bad351f2421684de23fba35a0d8fcdb7004c6bd
jotapemr <101481431+jotapemr@users.noreply.github.com>
2022-03-26 22:21:25 -0300

    Minha primeira API Aula 4 parte final
    Essa foi minha primeira API

---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Controllers/Controller/UsuarioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaPrimeiraAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAPI.Controllers.NovaPasta
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]    //método get
+        public IActionResult ObterUsuario()
+        {
+            var usuario = new Usuario()
+            {
+                Nome = "Teste",
+                Email = "teste2@xxx.com",        //simulando um dado que veio de um banco de dados
+                Senha = "outroteste123123"
+            };
+            return Ok(usuario);
+
+            // passo a passo:
+            //criei uma API
+            // chamei essa API transferindo para um controlador
+            // dentro do controlador criei uma rota por um método HTTP
+            // criei um método para tratar essa rota
+            // estenciei um usuário (simulei como se tivesse puxado do banco de dados, e devolveu esse usuário
+            // e usei um framework para não voltar a senha para o usuário
+            
+        }
+    }
+}
---
+++ MinhaPrimeiraAPI/MinhaPrimeiraAPI/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace MinhaPrimeiraAPI.Models
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Email { get; set;}
+        [JsonIgnore]   //framework
+        public string Senha { get; set; }
+
+    }
+}
